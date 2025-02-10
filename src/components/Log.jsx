43d5324db3647a9d0a0d85e505e2c2665f9a0b30
Gustavo Tavares <gustavo.tavares.dev@outlook.com>
2025-02-09 22:20:51 -0300

    feat: added logs based on game turns

--- src/components/Log.jsx
+++ src/components/Log.jsx
@@ -2,7 +2,11 @@ export default function Log({ turns }) {
 
     return(
         <ol id="log">
-
+            {turns.map(turn =>
+                <li key={`${turn.square.row}${turn.square.column}`}>
+                    {turn.player} selected {turn.square.row},{turn.square.column}
+                </li>
+            )}
         </ol>
     )
 }
